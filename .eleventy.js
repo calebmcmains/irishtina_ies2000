91dc573476e117407bdac2dc8a857e15a2441636
calebmcmains <calebmcmains@gmail.com>
2024-01-29 00:12:05 -0700

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/public/css/");
+  eleventyConfig.addPassthroughCopy("src/public/images/");
+  eleventyConfig.addPassthroughCopy("src/public/js/");
+  eleventyConfig.addPassthroughCopy({
+    "global.out.css": "global.css",
+  });
+
+  eleventyConfig.addWatchTarget("src/css/");
+  eleventyConfig.addWatchTarget("src/css/custom.min.css");
+  eleventyConfig.addWatchTarget("src/css/custom.min.css.map");
+  eleventyConfig.addWatchTarget("src/scss/");
+  eleventyConfig.addWatchTarget("src/scss/custom.scss/");
+  eleventyConfig.addWatchTarget("src/js/");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site",
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};

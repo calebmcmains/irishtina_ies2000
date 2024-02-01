1a2061e107a5b4fde78603d632536cfbba26e3f1
calebmcmains <calebmcmains@gmail.com>
2024-02-01 00:15:59 -0700

    mostly finished landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
         iesGreen: "#39b54a",
         "iesGreen-light": "#61c46e",
         "iesGreen-lightest": "#ebf8ed",
+        "slightly-white": "#fafafa",
+      },
+      backgroundImage: {
+        "hero-image":
+          "url('/public/images/filip-barna-SlIu4D_rTPo-unsplash.jpg')",
+      },
+      scale: {
+        1025: "1.025",
       },
     },
   },
